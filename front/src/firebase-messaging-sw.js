f7d346f784c70383f892825cd0765fe4f90567aa
kbmplaying <kaleth.bahena@zabud.com.co>
2022-06-02 17:53:02 -0500

    adding vscode to gitignore file

--- front/src/firebase-messaging-sw.js
+++ front/src/firebase-messaging-sw.js
@@ -2,12 +2,12 @@ importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-app-compat.js")
 importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDHcMQ2xc8oO-5Nf87AfxioRY_Vy1uK90Y",
-  authDomain: "notification-push-3973e.firebaseapp.com",
-  projectId: "notification-push-3973e",
-  storageBucket: "notification-push-3973e.appspot.com",
-  messagingSenderId: "717966679749",
-  appId: "1:717966679749:web:3a19daeee88360dc871aac"
+  apiKey: "XXXXXXXXXXXXXXXXXXX",
+  authDomain: "XXXXXXXXXXXXXXXXXXX",
+  projectId: "XXXXXXXXXXXXXXXXXXX",
+  storageBucket: "XXXXXXXXXXXXXXXXXXX",
+  messagingSenderId: "XXXXXXXXXXXXXXXXXXX",
+  appId: "XXXXXXXXXXXXXXXXXXX"
 })
 
 const messaging = firebase.messaging();
